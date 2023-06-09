0ef55ac414ea5c22b366a641f31cd1cc70ed09a8
chenp6 <52535982+chenp6@users.noreply.github.com>
2023-06-10 03:50:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ app.listen(5000, () => {
     console.log(new Date() + "開始監聽port 5000!");
 });
 
-app.get("/username", async(req, res) => {
-    const email = req.query.email;
+app.get("/users/:email", async(req, res) => {
+    const email = req.params.email;
     if (email == 'test@gmail.com') {
         return res.status(200).json({ 'username': 'testing' });
     } else {
